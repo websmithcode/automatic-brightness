e78754cab10b24e8c593f04c22de8b5154319c3b
websmithcode <websmithcode@gmail.com>
2024-09-20 16:52:09 +0300

    fix: :zap: Fix CPU usage bug

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,12 @@ async fn set_brightness(val: u32) -> Result<(), brightness::Error> {
 fn compute_brightness() -> u32 {
     let now = Local::now();
     let hour = now.hour();
-
     return match hour {
         6..=9 => 80,
-        10..=15 => 100,
-        16..=17 => 60,
-        18..=19 => 50,
-        20..=22 => 30,
+        10..=17 => 100,
+        18..=19 => 60,
+        20..=21 => 50,
+        22 => 30,
         23 => 20,
         _ => 0,
     };
@@ -35,13 +34,11 @@ fn main() {
     loop {
         let brightness = compute_brightness();
 
-        if prev_brightness == Some(brightness) {
-            continue;
+        if prev_brightness != Some(brightness) {
+            block_on(set_brightness(brightness)).unwrap();
+            prev_brightness = Some(brightness);
         }
 
-        block_on(set_brightness(brightness)).unwrap();
-
-        prev_brightness = Some(brightness);
         thread::sleep(Duration::from_secs(10));
     }
 }
